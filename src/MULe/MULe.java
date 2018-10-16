4a03f5d9421524f672e64f4277707fad1e30d739
gmm96 <guillermo10_96@hotmail.com>
2018-10-16 20:19:14 +0200

    Uploading project

---
+++ src/MULe/MULe.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package MULe;
+//import MULe.View;
+
+public class MULe {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        View view = new View();
+    }
+    
+}
